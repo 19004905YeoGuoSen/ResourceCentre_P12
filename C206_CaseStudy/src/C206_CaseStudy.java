301336c4f83a220798bd9f3a86e06458cbfd8807
19040971 <19040971@DESKTOP-2OPUF9T>
2020-08-17 11:44:31 +0800

    Created all methods for Orders

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -14,13 +14,51 @@ public class C206_CaseStudy {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
 			
-
+			if(option == 1) {
+				order();
+				int orders = Helper.readInt("Enter option to select > ");
+				
+				if(orders == 1) {
+					C206_CaseStudy.viewAllOrder(orderList);
+				} else if (orders == 2) {
+					C206_CaseStudy.addAllOrder(orderList);
+				} else if (orders == 3) {
+					C206_CaseStudy.deleteAllOrder(orderList);
+				}
+			
+			} 
 
 	}
 
 }
 
-//----------------------------------------------menu()---------------------------------------------------------------
+
+//===================================================Order=========================================================
+	private static void viewAllOrder(ArrayList<Order> orderList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private static void addAllOrder(ArrayList<Order> orderList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void deleteAllOrder(ArrayList<Order> orderList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private static void order() {
+		// TODO Auto-generated method stub
+		System.out.println("1. View orders");
+		System.out.println("2. Add orders");
+		System.out.println("3. Delete orders");
+	}
+
+//===================================================menu()=========================================================
 	private static void menu() {
 		// TODO Auto-generated method stub
 		C206_CaseStudy.setHeader("SCHOOL LUNCH APP");
